929cb40acee25309b05cd6fb679c2a9b2f8e4077
srimayee madasu <smadasu@fibonaccisolutions.com>
2019-09-07 16:49:16 -0400

    new challenges

---
+++ src/ReverseNumber.java
@@ -0,0 +1,14 @@
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+
+		int a = 12345;
+		
+		String string = String.valueOf(a);
+		string.chars().forEach((c) -> {
+			
+		});
+	}
+
+}
